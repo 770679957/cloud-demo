e1a3b04d63a7ba7b58ba19fe68c965c3e218eed6
yyw <1900205783@qq.com>
2019-08-26 01:42:29 +0800

    item 通过config server 成功读取github 上的配置文件。

--- demo-microservice-item/src/main/java/cn/yyw/microservice/controller/ItemController.java
+++ demo-microservice-item/src/main/java/cn/yyw/microservice/controller/ItemController.java
@@ -2,6 +2,7 @@ package cn.yyw.microservice.controller;
 
 
 import cn.yyw.microservice.pojo.Item;
+import cn.yyw.microservice.pojo.JdbcConfigBean;
 import cn.yyw.microservice.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,10 +19,19 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
-    // 127.0.0.1:8081/order/59193738268961441
+    @Autowired
+    private JdbcConfigBean jdbcConfigBean;
+
+    // 127.0.0.1:8081/item/1
     @GetMapping(value = "/item/{id}")
     public Item queryItemById(@PathVariable("id") Long id) {
         return this.itemService.queryItemById(id);
+    }
+
+    // 127.0.0.1:8081/test
+    @GetMapping(value = "test")
+    public String test() {
+        return this.jdbcConfigBean.toString();
     }
 
 }
